c4142a7cfb683b43098e64e0ca8fddc76cbfd1a7
iamtekson <teksondada@gmail.com>
2019-11-28 16:59:40 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);

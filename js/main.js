7341177bf4408ef60ff3b3ed7070a2e3d1c4baf5
iamtekson <teksondada@gmail.com>
2019-11-28 17:13:26 +0700

    geojson data added to project folder

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//map variable
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+// tile layers
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
+//print function in map
 L.control.browserPrint({
     title: 'Print current Layer',
     documentTitle: 'Utility Management System',
@@ -18,3 +22,5 @@ L.control.browserPrint({
     manualMode: false,
     closePopupsOnPrint: true, //default value
 }).addTo(map);
+
+L.geoJSON('data/data').addTo(map)

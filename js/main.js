54eef1ec6377cc06ce54e055bb63214e801fec05
iamtekson <teksondada@gmail.com>
2019-11-29 14:00:36 +0700

    ajax request to fetch the data

--- js/main.js
+++ js/main.js
@@ -19,7 +19,39 @@ L.control.browserPrint({
     closePopupsOnPrint: true, //default value
 }).addTo(map);
 
-$.getJSON('data/jamoat.json', function (data) {
+
+//get Geojson Locally
+// $.getJSON('data/jamoat.json', function (data) {
+//     console.log(data)
+//     new L.GeoJSON(data).addTo(map)
+// })
+
+
+url = "http://localhost:8080/geoserver/generateMap/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=generateMap%3Ajamoat&maxFeatures=50&outputFormat=application%2Fjson"
+// ajax request handler 
+function handleAjax() {
+    //Geoserver Web Feature Service
+    $.ajax('http://localhost:8080/geoserver/wfs', {
+        type: 'GET',
+        data: {
+            service: 'WFS',
+            version: '1.1.0',
+            request: 'GetFeature',
+            typename: 'generateMap:jamoat',
+            CQL_FILTER: `jamoat='Bobojon_Ghafurov_Chshmasor'`,
+            srsname: 'EPSG:4326',
+            outputFormat: 'text/javascript',
+        },
+        dataType: 'jsonp',
+        jsonpCallback: 'callback:handleJson',
+        jsonp: 'format_options'
+    });
     console.log(data)
-    new L.GeoJSON(data).addTo(map)
-})
+}
+
+// the ajax callback function
+function handleJson(data) {
+    selectedArea = L.geoJson(data).addTo(map);
+    map.fitBounds(selectedArea.getBounds());
+}
+handleAjax()
